c8a610f7ccb00370d0f96b667dcfe6e183d9ce80
Daniverd15 <dvillamizar435@unab.edu.co>
2025-11-03 22:32:15 -0500

    Inicio tutorial multiplataformeo

---
+++ Assets/New_Character/New_CharacterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof(CharacterController))]
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public float WalkSppeed = 4f;
+    public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
